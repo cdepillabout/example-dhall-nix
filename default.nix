66ea6baf2623e5b52411dcab237ebbbbb4d369d4
Dennis Gosnell <cdep.illabout@gmail.com>
2021-09-27 14:49:49 +0900

    Add all dependencies for buildDhallDirectoryPackage.

--- default.nix
+++ default.nix
@@ -105,7 +105,23 @@ let
       source = false;
       document = false;
       dependencies = [
-        myFODDhallFile
+        (buildDhallUrl {
+          url = "https://raw.githubusercontent.com/cdepillabout/example-dhall-repo/c1b0d0327146648dcf8de997b2aa32758f2ed735/example1.dhall";
+          hash = "sha256-ZTSiQUXpPbPfPvS8OeK6dDQE6j6NbP27ho1cg9YfENI=";
+          dhall-hash = "sha256:6534a24145e93db3df3ef4bc39e2ba743404ea3e8d6cfdbb868d5c83d61f10d2";
+        })
+        (buildDhallUrl {
+          url = "https://raw.githubusercontent.com/dhall-lang/dhall-lang/9758483fcf20baf270dda5eceb10535d0c0aa5a8/Prelude/List/map.dhall";
+          hash = "sha256-3YRf+0Vo1AMn8qgX60LRxhOLkpynWNULwzES7zyIVoA=";
+          dhall-hash = "sha256:dd845ffb4568d40327f2a817eb42d1c6138b929ca758d50bc33112ef3c885680";
+        })
+
+        # This is used in ./upperAndDouble.dhall.
+        (buildDhallUrl {
+          url = "https://raw.githubusercontent.com/dhall-lang/dhall-lang/9758483fcf20baf270dda5eceb10535d0c0aa5a8/Prelude/Text/upperASCII.dhall";
+          hash = "sha256-Ra5PvYFLBHTmXCik7pKyO5eYkvpbtzcwvJlnWueQyik=";
+          dhall-hash = "sha256:45ae4fbd814b0474e65c28a4ee92b23b979892fa5bb73730bc99675ae790ca29";
+        })
       ];
     };
 
@@ -116,6 +132,4 @@ let
 
 in
 
-# myDhallPackage
-
-myFODDhallFile
+myDhallPackage
